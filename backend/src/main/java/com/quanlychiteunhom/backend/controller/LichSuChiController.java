cb7c03a84de02bbb0b17c08d5d80ebe751069b9b
Tuyen <tuyennguyen03092003@gmail.com>
2024-06-10 00:45:18 +0700

    Update

--- backend/src/main/java/com/quanlychiteunhom/backend/controller/LichSuChiController.java
+++ backend/src/main/java/com/quanlychiteunhom/backend/controller/LichSuChiController.java
@@ -3,37 +3,43 @@ package com.quanlychiteunhom.backend.controller;
 import com.quanlychiteunhom.backend.entities.LichSuChi;
 import com.quanlychiteunhom.backend.services.LichSuChiService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/lich_su_chi")
+@RequestMapping("/api")
 public class LichSuChiController {
     @Autowired
     private LichSuChiService lichSuChiService;
 
-    @GetMapping
-    public List<LichSuChi> getAllLichSuChi()
-    {
-        return lichSuChiService.getAllLichSuChi();
-    }
 
-    @GetMapping ("/{id}")
-    public LichSuChi getLichSuChiById(@PathVariable Long id)
-    {
-        return lichSuChiService.getLichSuChiById(id);
+//    @GetMapping
+//    public List<LichSuChi> getAllLichSuChi()
+//    {
+//        return lichSuChiService.getAllLichSuChi();
+//    }
+//
+//    @GetMapping ("/{id}")
+//    public LichSuChi getLichSuChiById(@PathVariable Long id)
+//    {
+//        return lichSuChiService.getLichSuChiById(id);
+//    }
+//
+//    @PostMapping
+//    public  LichSuChi createLichSuChi(@RequestBody LichSuChi lichSuChi)
+//    {
+//        return lichSuChiService.createLichSuChi(lichSuChi);
+//    }
+    @PostMapping ("/chi")
+    public ResponseEntity<Void> createChi(@RequestBody LichSuChi chi) {
+        // Lưu chi vào database
+        return ResponseEntity.ok().build();
     }
-
-    @PostMapping
-    public  LichSuChi createLichSuChi(@RequestBody LichSuChi lichSuChi)
-    {
-        return lichSuChiService.createLichSuChi(lichSuChi);
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteLichSuChi(@PathVariable Long id)
-    {
-        lichSuChiService.deleteLichSuChi(id);
-    }
+//    @DeleteMapping("/{id}")
+//    public void deleteLichSuChi(@PathVariable Long id)
+//    {
+//        lichSuChiService.deleteLichSuChi(id);
+//    }
 }
--- backend/src/main/java/com/quanlychiteunhom/backend/controller/LichSuThuController.java
+++ backend/src/main/java/com/quanlychiteunhom/backend/controller/LichSuThuController.java
@@ -3,37 +3,43 @@ package com.quanlychiteunhom.backend.controller;
 import com.quanlychiteunhom.backend.entities.LichSuThu;
 import com.quanlychiteunhom.backend.services.LichSuThuService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/lich_su_thu")
+@RequestMapping("/api")
 public class LichSuThuController {
     @Autowired
     private LichSuThuService LichSuThuService;
 
-    @GetMapping
-    public List<LichSuThu> getAllLichSuThu()
-    {
-        return LichSuThuService.getAllLichSuThu();
-    }
-
-    @GetMapping ("/{id}")
-    public LichSuThu getLichSuThuById(@PathVariable Long id)
-    {
-        return LichSuThuService.getLichSuThuById(id);
-    }
-
-    @PostMapping
-    public  LichSuThu createLichSuThu(@RequestBody LichSuThu LichSuThu)
-    {
-        return LichSuThuService.createLichSuThu(LichSuThu);
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteLichSuThu(@PathVariable Long id)
-    {
-        LichSuThuService.deleteLichSuThu(id);
+//    @GetMapping
+//    public List<LichSuThu> getAllLichSuThu()
+//    {
+//        return LichSuThuService.getAllLichSuThu();
+//    }
+//
+//    @GetMapping ("/{id}")
+//    public LichSuThu getLichSuThuById(@PathVariable Long id)
+//    {
+//        return LichSuThuService.getLichSuThuById(id);
+//    }
+//
+//    @PostMapping
+//    public  LichSuThu createLichSuThu(@RequestBody LichSuThu LichSuThu)
+//    {
+//        return LichSuThuService.createLichSuThu(LichSuThu);
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public void deleteLichSuThu(@PathVariable Long id)
+//    {
+//        LichSuThuService.deleteLichSuThu(id);
+//    }
+    @PostMapping("/thu")
+    public ResponseEntity<Void> createThu(@RequestBody LichSuThu thu) {
+        // Lưu thu vào database
+        return ResponseEntity.ok().build();
     }
 }
