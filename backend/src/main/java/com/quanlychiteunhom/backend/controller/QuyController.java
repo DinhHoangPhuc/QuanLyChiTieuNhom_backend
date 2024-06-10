6392bdd68ea51592e6bdc9859ced7eed238c3c20
Dinh Hoang Phuc <98966221+DinhHoangPhuc@users.noreply.github.com>
2024-06-10 23:06:21 +0700

    hoàn thành get quỹ by nhóm

--- backend/src/main/java/com/quanlychiteunhom/backend/controller/QuyController.java
+++ backend/src/main/java/com/quanlychiteunhom/backend/controller/QuyController.java
@@ -2,9 +2,11 @@ package com.quanlychiteunhom.backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.quanlychiteunhom.backend.dto.QuyRequest;
@@ -20,5 +22,10 @@ public class QuyController {
     @PostMapping("/taoQuy")
     public ResponseEntity<?> taoQuy(@RequestBody QuyRequest quyRequest) {
         return quyService.addQuy(quyRequest);
+    }
+
+    @GetMapping("/getQuy")
+    public ResponseEntity<?> getQuy(@RequestParam int nhomId) {
+        return quyService.getQuy(nhomId);
     }
 }
